ad56c17cd8f24db81d88b31495b12fae8d676777
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-02-09 13:24:58 +0100

    Regex Match check system

--- DBSearch/obj/Release/net8.0/DBSearch.AssemblyInfo.cs
+++ DBSearch/obj/Release/net8.0/DBSearch.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBSearch")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b983bd782a272017b43a20615969befcccc7c14e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBSearch")]
