2ba19aed8028f8453f6c6a0c1bfabcd71462d4d0
additivepill62 <gabordaniel175@gmail.com>
2026-02-10 21:01:41 +0100

    DatabaseServices osztály újragondolása és hozzáigazítása a program.cs-hez

--- DBSearch/Model/DatabaseServices.cs
+++ DBSearch/Model/DatabaseServices.cs
@@ -1,190 +1,83 @@
 ﻿using MySqlConnector;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DBSearch.model
 {
 	internal class DatabaseServices
 	{
-		private static string connectionString;
-		private static string table;
-		private static string queryParameters;
-
-		public static void DbConnectionCheck(string connectionString)
+		public static string typeDecider(string userInput)
 		{
-			try
-			{
-				using (MySqlConnection connection = new MySqlConnection(connectionString))
-				{
-					connection.Open();
-					Console.WriteLine("Successful connection to database");
-				}
-			}
-			catch (Exception error)
-			{
-				Console.WriteLine("Unsuccesful connection to database");
-				Console.WriteLine(error);
-			}
+			if (string.IsNullOrWhiteSpace(userInput))
+				return null;
 
+			if (userInput.Contains("@"))
+				return "email";
+			if (userInput.Contains("+"))
+				return "phone_number";
+			if (userInput.Contains(" "))
+				return "full_name";
 
-
-
+			return null;
 		}
-
 
 		public static DataTable DBsearch(string tableName, string connectionString, string userInput)
 		{
+			string column = typeDecider(userInput);
+			if (column == null)
+				throw new Exception("Hibás keresési formátum");
+
 			using var connection = new MySqlConnection(connectionString);
 			connection.Open();
-			
 
-			using var command = new MySqlCommand($"select * from {tableName} where {typeDecider(userInput)} = {userInput}", connection);
+			using var command = new MySqlCommand(
+				$"SELECT * FROM {tableName} WHERE {column} = @value",
+				connection
+			);
+
+			command.Parameters.AddWithValue("@value", userInput);
 
 			using var reader = command.ExecuteReader();
-			var dataTable = new DataTable();
+			var table = new DataTable();
+			table.Load(reader);
 
-			dataTable.Load(reader);
-
-
-			return dataTable;
+			return table;
 		}
-		
-		
-		public static DataTable DBdelete(string tableName, string connectionString, string userInput)
+
+		public static int DBdelete(string tableName, string connectionString, string userInput)
+		{
+			string column = typeDecider(userInput);
+			if (column == null)
+				throw new Exception("Hibás törlési formátum");
+
+			using var connection = new MySqlConnection(connectionString);
+			connection.Open();
+
+			using var command = new MySqlCommand(
+				$"DELETE FROM {tableName} WHERE {column} = @value",
+				connection
+			);
+
+			command.Parameters.AddWithValue("@value", userInput);
+
+			return command.ExecuteNonQuery();
+		}
+
+		public static int DBadd(string connectionString, string fullName, string email, string phoneNumber)
 		{
 			using var connection = new MySqlConnection(connectionString);
 			connection.Open();
-			
 
-			using var command = new MySqlCommand($"delete from {tableName} where {typeDecider(userInput)} = {userInput}", connection); 
+			using var command = new MySqlCommand(
+				"INSERT INTO users (full_name, email, phone_number) VALUES (@name, @email, @phone)",
+				connection
+			);
 
-			using var reader = command.ExecuteReader();
-			var dataTable = new DataTable();
+			command.Parameters.AddWithValue("@name", fullName);
+			command.Parameters.AddWithValue("@email", email);
+			command.Parameters.AddWithValue("@phone", phoneNumber);
 
-			dataTable.Load(reader);
-
-
-			return dataTable;
-		}
-		public static DataTable BDadd(string tableName, string connectionString, string userInput, string fullName, string email, string phoneNumber)
-		{
-			using var connection = new MySqlConnection(connectionString);
-			connection.Open();
-			
-
-			using var command = new MySqlCommand($"insert into users (full_name, email, phone_number) values ('{fullName}', '{email}', '{phoneNumber}');", connection); 
-
-			using var reader = command.ExecuteReader();
-			var dataTable = new DataTable();
-
-			dataTable.Load(reader);
-
-
-			return dataTable;
-		}
-
-
-
-		public static string typeDecider(string userInput)
-		{
-			string type = "";
-			if (userInput.Contains("@"))
-			{
-				type = "email";
-			}
-			else if (userInput.Contains("+"))
-			{
-				type = "phone_number";
-			}
-			else if (userInput.Contains(" "))
-			{
-				type = "full_name";
-			}
-			else
-			{
-				Console.WriteLine("Wrong format! Try again");  //NOTE: hibát le kell kezelni, hogy újra megkérje a felhasználót a helyes formátumú inputra, jelenleg csak visszatér null-lal, ami nem jó
-				return null;
-			}
-			return type;
-		}
-
-
-		/*
-		public static DataTable DBFunctions(string tableName, string connectionString, string userInput, int userChoice)
-		{
-			using var connection = new MySqlConnection(connectionString);
-			connection.Open();
-			string tipus = "";
-
-
-			if (userInput.Contains("@"))
-			{
-				tipus = "email";
-			}
-			else if (userInput.Contains("+"))
-			{
-				tipus = "phone_number";
-			}
-			else if(userInput.Contains(" "))
-			{
-				tipus = "full_name";
-			}
-			else
-			{
-				Console.WriteLine("Wrong format! Try again");
-				return null;
-			}
-
-			
-				switch (userChoice)
-				{
-					case 1:
-					MySqlCommand command = new MySqlCommand($"select * from {tableName} where {tipus} = {userInput}", connection);
-						break;
-					case 2:
-					MySqlCommand commandx = new MySqlCommand($"insert into users (full_name, email, phone_number) values ('Lenard Easterbrook', 'leasterbrook0@parallels.com', '+36 30 723 7523');", connection);
-						break;
-					case 3:
-					MySqlCommand commandy = new MySqlCommand($"delete from {tableName} where {tipus} = {userInput}", connection);
-						break;
-				}
-
-
-
-
-
-
-
-			using var nameCommand = new MySqlCommand($"select * from {tableName}  where {tipus} = {userInput}", connection);
-
-			using var reader = nameCommand.ExecuteReader();
-			var dataTable = new DataTable();
-
-			dataTable.Load(reader);
-
-		
-			return dataTable;
-		}*/
-		
-
-
-		public static DataTable GetAllData(string tableName, string connectionString)
-		{
-			using var connection = new MySqlConnection(connectionString);
-			connection.Open();
-			using var command = new MySqlCommand($"select * from " + tableName, connection);
-
-			using var reader = command.ExecuteReader();
-			var dataTable = new DataTable();
-
-			dataTable.Load(reader);
-
-
-			return dataTable;
+			return command.ExecuteNonQuery();
 		}
 	}
 }
